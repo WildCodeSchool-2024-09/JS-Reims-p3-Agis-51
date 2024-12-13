90f3b80abb89d34174f7204fc2257f9c6dafe5a6
begie1 <jbergeline1@gmail.com>
2024-12-13 12:19:41 +0100

    Createvehicle fonctionne

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,16 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-
 import documentActions from "./modules/document/documentActions";
-
 import vehicleActions from "./modules/vehicles/vehicleActions";
+
 router.get("/api/vehicles", vehicleActions.browse);
 router.get("/api/vehicles/:id", vehicleActions.read);
-
 router.post("/api/vehicles", vehicleActions.add);
-
 router.get("/api/document", documentActions.browse);
-
 
 export default router;
