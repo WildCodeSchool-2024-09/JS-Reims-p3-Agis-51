9827369484e63c1cf6f6abb113d2efdd9999b592
begie1 <jbergeline1@gmail.com>
2025-01-07 22:35:02 +0100

    modif home

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,15 +1,14 @@
 import "./Home.css";
+import banner from "../../assets/images/banner.jpg";
 
 const Home = () => {
   return (
     <div className="banner-images">
-      <img
-        src="/assets/images/banner.jpg"
-        alt="Banner"
-        className="banner-icon"
-      />
+      <div>
+        <img src={banner} alt="Banner" className="banner-icon" />
+      </div>
       <div className="info">
-        <h1> "Bienvenue à l'AGIS 51" </h1>
+        <h1> AGIS 51 </h1>
         <p>
           L’Association Garage pour l’Insertion et la Solidarité 51 a été créée
           en 2013 et son atelier de maintenance et de réparation automobile
@@ -20,8 +19,8 @@ const Home = () => {
           l’association s’est donnée une double mission :
         </p>
         <ul>
-          <li>l’insertion professionnelle</li>
-          <li>la mobilité pour l’emploi</li>
+          <li>L’insertion professionnelle</li>
+          <li>La mobilité pour l’emploi</li>
         </ul>
         <p>Ce projet est soutenu par l’Union Européenne.</p>
       </div>
